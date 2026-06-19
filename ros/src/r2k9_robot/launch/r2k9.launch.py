67b71189be871b02549dc82feb4cae6503c625c9
Steve Austin <trans.medium@gmail.com>
2026-06-19 10:59:47 -0700

    also launch rosbridge

--- ros/src/r2k9_robot/launch/r2k9.launch.py
+++ ros/src/r2k9_robot/launch/r2k9.launch.py
@@ -1,7 +1,10 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import AnyLaunchDescriptionSource
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
@@ -15,10 +18,18 @@ def generate_launch_description():
         'frame_height', default_value='480', description='Camera frame height'
     )
 
+    rosbridge_dir = get_package_share_directory('rosbridge_server')
+    rosbridge_launch = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            os.path.join(rosbridge_dir, 'launch', 'rosbridge_websocket_launch.xml')
+        )
+    )
+
     return LaunchDescription([
         device_id_arg,
         frame_width_arg,
         frame_height_arg,
+        rosbridge_launch,
         Node(
             package='r2k9_robot',
             executable='dpad_logger',
@@ -29,6 +40,12 @@ def generate_launch_description():
             package='r2k9_robot',
             executable='immobility_monitor',
             name='object_immobility_monitor',
+            output='screen',
+        ),
+        Node(
+            package='r2k9_robot',
+            executable='kobuki_controller',
+            name='kobuki_controller_node',
             output='screen',
         ),
         Node(
