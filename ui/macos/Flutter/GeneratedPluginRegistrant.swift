7c9155e02a31c48efc1e237da0ada6e9a442b5cc
Steve Austin <trans.medium@gmail.com>
2026-06-23 23:41:10 -0700

    WIP fix

--- ui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_sound
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterSoundPlugin.register(with: registry.registrar(forPlugin: "FlutterSoundPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
