8ddaa1c97187ba75ec1524f8516f7fb2e6860c23
Steve Austin <kn6orm@gmail.com>
2026-07-02 16:01:57 -0700

    launch_r2k9.bash

--- tools/launch_r2k9.bash
+++ tools/launch_r2k9.bash
@@ -20,13 +20,7 @@ fi
 # 2. Compile the ROS workspace
 cd ros
 git submodule update --init --recursive
-touch src/aws_robomaker_bookstore_world/COLCON_IGNORE
-touch src/aws-robomaker-racetrack-world/COLCON_IGNORE
-touch src/rplidar_ros/COLCON_IGNORE
 colcon build --symlink-install --cmake-args -DCMAKE_CXX_FLAGS="-Wno-error=overloaded-virtual"
-rm src/aws_robomaker_bookstore_world/COLCON_IGNORE
-rm src/aws-robomaker-racetrack-world/COLCON_IGNORE
-rm src/rplidar_ros/COLCON_IGNORE
 source install/setup.bash
 
 
@@ -35,7 +29,7 @@ CURRENT_USER=$(whoami)
 # Check if the user is r2k9
 screen -LdmS kobuki ros2 launch kobuki kobuki.launch.py
 if [ "$CURRENT_USER" = "r2k9" ]; then
-    r2k9_robot ros2 launch r2k9_robot r2k9.launch.py
+    ros2 launch r2k9_robot r2k9.launch.py
 else
     screen -LdmS r2k9_robot ros2 launch r2k9_robot r2k9.launch.py
 fi
